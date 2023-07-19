aa9f0acea54eb5958e25fcb17de445fef06567db
Brian <wamwangimathenge@gmail.com>
2023-07-19 14:44:44 +0300

    Test if a given number is odd
    This commit writes a small program that takes a number and determines
    if it's an even number using the modulus (%) operator.

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ console.log("Mid-level skills:", score > 0 && score < 10);
 var timeRemaining = 0;
 var energy = 0;
 console.log("Game over:", timeRemaining == 0 || energy == 0);
+
+// Task 3
+var num1 = 9;
+var num2 = 8;
+var test1 = num1 % 2;
+var test2 = num2 % 2;
+var result1 = (test1 == 0);
+var result2 = (test2 == 0);
+
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2)
